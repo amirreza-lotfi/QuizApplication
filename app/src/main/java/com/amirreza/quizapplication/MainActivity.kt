c9ea9e5e42d767d8127b1bb1e09b6a62aa41f0d1
amirreza <amirreza.lotfi79@gmail.com>
2023-01-25 13:52:20 +0330

    setup navController

--- app/src/main/java/com/amirreza/quizapplication/MainActivity.kt
+++ app/src/main/java/com/amirreza/quizapplication/MainActivity.kt
@@ -2,11 +2,25 @@ package com.amirreza.quizapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import com.amirreza.quizapplication.feature_authentication.domain.entitiy.TokenHolder
 import com.amirreza.quizeapplication.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // if log in -> go to quiz fragment
+        // else -> go to log in  fragment
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+        val hasUserLoggedIn = TokenHolder.isTokenAvailable()
+
+        if(hasUserLoggedIn){
+            navController.navigate(R.id.quizFragment)
+        }else{
+            navController.navigate(R.id.loginFragment)
+        }
     }
 }
