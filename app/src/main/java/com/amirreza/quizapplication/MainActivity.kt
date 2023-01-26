d1e4a7550f9ecae9ceb734fe83716cc9c580fce4
amirreza <amirreza.lotfi79@gmail.com>
2023-01-26 12:43:36 +0330

    fix navigation pathes in homeFragment

--- app/src/main/java/com/amirreza/quizapplication/MainActivity.kt
+++ app/src/main/java/com/amirreza/quizapplication/MainActivity.kt
@@ -10,15 +10,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
-        val navController = navHostFragment.navController
-        val hasUserLoggedIn = TokenHolder.isTokenAvailable()
-
-//        if(hasUserLoggedIn){
-            navController.navigate(R.id.quizFragment)
-//        }else{
-//            navController.navigate(R.id.loginFragment)
-//        }
     }
 }
--- app/src/main/java/com/amirreza/quizapplication/feature_quiz/presentation/fragment_home/HomeFragment.java
+++ app/src/main/java/com/amirreza/quizapplication/feature_quiz/presentation/fragment_home/HomeFragment.java
@@ -5,12 +5,14 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.amirreza.quizapplication.feature_authentication.domain.entitiy.TokenHolder;
 import com.amirreza.quizapplication.util.base.QuizBaseFragment;
 import com.amirreza.quizeapplication.R;
 import com.amirreza.quizeapplication.databinding.FragmentHomeBinding;
@@ -29,6 +31,8 @@ public class HomeFragment extends QuizBaseFragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        checkIsItNeedToNavigateToAuthenticationScreen(view);
+
         binding.examHistoryBtn.setOnClickListener(view1->{
             Navigation.findNavController(view).navigate(R.id.action_homeFragment_to_quizFragment);
         });
@@ -36,5 +40,16 @@ public class HomeFragment extends QuizBaseFragment {
         binding.examHistoryBtn.setOnClickListener(view1 -> {
             Navigation.findNavController(view).navigate(R.id.action_homeFragment_to_historyFragment);
         });
+    }
+
+    public void checkIsItNeedToNavigateToAuthenticationScreen(View view){
+        NavController navController = Navigation.findNavController(view);
+        boolean hasUserLoggedIn = TokenHolder.INSTANCE.isTokenAvailable();
+
+        if(hasUserLoggedIn){
+            navController.navigate(R.id.quizFragment);
+        }else{
+            navController.navigate(R.id.loginFragment);
+        }
     }
 }
